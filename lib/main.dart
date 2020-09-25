47ca6e32fe9a022eaefd530cfcdb134e5975f43a
Hashir Umar <hashirumar19@gmail.com>
2020-09-25 11:55:59 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("World Time App"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Center(
+        child: Text("Hello world")
+      ))
+  )
+  );
+}
